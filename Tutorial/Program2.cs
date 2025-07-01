f0c90851121fdb9255c4d4d97f97cd07aa077324
Administrator <Administrator@DESKTOP-RSNG7DD>
2025-07-01 15:53:16 +0530

    Add project files.

---
+++ Tutorial/Program2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Name: VYAS RAJAN");
+            Console.WriteLine("DOB: 15 / 10 / 2003");
+            Console.WriteLine("Address: 4, xyx society,");
+            Console.WriteLine("            Kalawad Road");
+            Console.WriteLine("City: Rajkot");
+            Console.WriteLine("Pincode: 360 001");
+            Console.WriteLine("State: Gujarat");
+            Console.WriteLine("Country: India");
+            Console.WriteLine("Email: rajan@ymail.com");
+        }
+    }
+}
